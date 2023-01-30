6e97023015bb814cb9900567a61a977a0d5930e6
Teodora Stojanovic <teacine6@gmail.com>
2023-01-30 23:26:43 +0100

    dodata pocetna stranica

---
+++ src/komponente/Pocetna.jsx
@@ -0,0 +1,38 @@
+function Pocetna(){
+
+return(
+<div  className='pocetna'>
+  
+<div className="hero-text">
+    <h1  > Zabavi se!</h1>
+</div>
+
+<br /><br /><br /><br /><br />
+<div className="flex-container">
+    <div>
+        <h2>Tehno</h2>
+        <p>Tehno je oblik elektronske muzike, koji se razvio u Detroitu (Mičigen, SAD) sredinom 1980, pod uticajem elektro i fank muzike, kao i novog talasa. </p>
+    </div>
+    <div  >          
+        <h2>Trance</h2>
+        <p> Nastao je na prelazu 1980. и 1990, kao kombinacija tehno i haus muzike. Frankfurt na Majni se smatra njegovim mestom rodjenjem. Muziku karakterise tempo od 130 do 160 bpm</p>
+    </div>
+    <div >         
+        <h2>House</h2> 
+        <p>House je muzicki stil elektronske muzike. Nastao je sredinom osamdesetih godina u Cikagu, umiksavanjem disko muzike iz sedamdesetih</p>
+    </div>  
+
+</div>
+
+</div>
+
+
+);
+}
+
+export default Pocetna;
+
+
+
+
+
